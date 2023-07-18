7c7dacefb7b3c56f4a81d98d23fb68607215bd6d
Allen Fabag <glitzlhen124@gmail.com>
2023-07-19 03:44:12 +0800

    for the home page

---
+++ bookingproj_capstone_front/src/components/Home/HomePage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../../App.css';
+import HomeBody from '../HomeBody';
+
+
+function HomePage() {
+  return (
+    <>
+      <HomeBody />
+    </>
+  );
+}
+
+export default HomePage;
